bdc3f7aa4fa27687c38ec5c9d7f9f76775e08f51
Tyler Huang <thuang00@stuy.edu>
2019-10-22 08:50:07 -0400

    app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+#Team Fine Pizza avrahamiB huangT linW wangD
+#SoftDev1 pd2
+#P00: Da Art of Storytellin'
+#2019-10-28
+
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+app = Flask(__name__)
+app.secret_key = "adsfgt"
