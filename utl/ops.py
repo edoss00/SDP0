669a73153fc134bd68af001288971f7a445190c3
bavrahami00 <bavrahami00@stuy.edu>
2019-10-22 23:38:17 +0100

    Finished update command

--- utl/ops.py
+++ utl/ops.py
@@ -7,23 +7,32 @@ db = sqlite3.connect(dbfile)
 c = db.cursor()
 
 
-def update(table, edit):
-    current = "SELECT story_text FROM stories WHERE story_id = {}"
-    command = current.format()
+def update(story, edit):
+    current = ""
+    outline = "SELECT story_text FROM stories WHERE story_id = {};"
+    command = outline.format(story)
+    q = c.execute(command)
+    for bar in q:
+      current = bar[0] + edit
+    outline = "UPDATE stories SET story_text = {}, last_edit = {} WHERE story_id = {};"
+    command = outline.format(current, edit, story)
+ 
+    
+    
 
 
 def insert(table, param1, param2):
-    outline = "INSERT INTO {} VALUES({}, {})"
+    outline = "INSERT INTO {} VALUES({}, {});"
     command = outline.format(table, param1, param2)
     c.execute(command)
 
 def insert(table, param1, param2, param3):
-    outline = "INSERT INTO {} VALUES({}, {}, {})"
+    outline = "INSERT INTO {} VALUES({}, {}, {});"
     command = outline.format(table, param1, param2, param3)
     c.execute(command)
 
 def insert(table, param1, param2, param3, param4):
-    outline = "INSERT INTO {} VALUES({}, {}, {}, {})"
+    outline = "INSERT INTO {} VALUES({}, {}, {}, {});"
     command = outline.format(table, param1, param2, param3, param4)
     c.execute(command)
 
