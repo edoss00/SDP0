09ccd10207178ad2b5f9d7197b83c33f32c6a9b9
Tyler Huang <thuang00@stuy.edu>
2019-10-22 09:11:01 -0400

    modified templates and app.py

--- app.py
+++ app.py
@@ -4,19 +4,24 @@
 #2019-10-28
 
 from flask import Flask, render_template, request, session, redirect, url_for, flash
+import sqlite3
 app = Flask(__name__)
 app.secret_key = "adsfgt"
 
 session = {}
 
-@app.route()
+@app.route("/")
 def root(): #if user is logged in, redirect to the homepage, otherwise prompt user to login or register
-    return render_template("homepage.html")
+    return render_template("landingpage.html")
 
 @app.route("/home")
 def home(): #display home page of website
-    a
+    return render_template("homepage.html")
 
-@app.rout("/logout")
+@app.route("/logout")
 def logout(): #logs out user, return to login/register page
     return redirect(url_for("root"))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
