6da910187fd45afd068a6ff180d768fd81f365d6
bavrahami00 <bavrahami00@stuy.edu>
2019-10-22 09:22:09 -0400

    Overloaded ops method

--- utl/ops.py
+++ utl/ops.py
@@ -7,6 +7,21 @@ db = sqlite3.connect(dbfile)
 c = db.cursor()
 
 
+def insert(table, param1, param2):
+    outline = "INSERT INTO {} VALUES({}, {})"
+    command = outline.format(table, param1, param2)
+    c.execute(command)
+
+def insert(table, param1, param2, param3):
+    outline = "INSERT INTO {} VALUES({}, {}, {})"
+    command = outline.format(table, param1, param2, param3)
+    c.execute(command)
+
+def insert(table, param1, param2, param3, param4):
+    outline = "INSERT INTO {} VALUES({}, {}, {}, {})"
+    command = outline.format(table, param1, param2, param3, param4)
+    c.execute(command)
+
 
 db.commit()
 db.close()
