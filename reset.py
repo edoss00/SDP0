d4b9f9f13fa892f2fdb0008c84e97efcf7a3b1c9
William Lin <wlin01@stuy.edu>
2019-10-26 15:19:14 -0400

    updated

---
+++ reset.py
@@ -0,0 +1,24 @@
+import sqlite3   #enable control of an sqlite database
+
+DB_FILE = "holding.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+command = "DROP TABLE IF EXISTS users"
+c.execute(command)
+
+command = "DROP TABLE IF EXISTS stories"
+c.execute(command)
+
+command = "DROP TABLE IF EXISTS edits"
+c.execute(command)
+
+command = "CREATE TABLE IF NOT EXISTS users (user_id INTEGER, username TEXT, password TEXT);" # create table
+c.execute(command)    # run SQL statement
+
+command = "CREATE TABLE IF NOT EXISTS stories (story_id INTEGER, story_name TEXT, story_text TEXT, last_edit TEXT);" # create table
+c.execute(command)    # run SQL statement
+
+command = "CREATE TABLE IF NOT EXISTS edits (user_id INTEGER, story_id INTEGER);" # create table
+c.execute(command)    # run SQL statement
